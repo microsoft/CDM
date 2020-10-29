b13db1dcacd703461d64cb01fd80c43f32bc3c1e
Daniel Ciborowski <dciborow@microsoft.com>
2020-10-28 20:25:25 -0400

    Update setup.py

--- objectModel/Python/setup.py
+++ objectModel/Python/setup.py
@@ -67,8 +67,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
-    ],
-    cmdclass={
-        'copy_resources': CopyResourcesCommand()
-    }
+    ]
 )
