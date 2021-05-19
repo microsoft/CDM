bb26a9fc0dd4e28c8162f61b7cd96dadc3a8e760
Daniel Ciborowski <dciborow@microsoft.com>
2021-05-19 16:10:42 -0400

    update attrs for pytest

--- objectModel/Python/setup.py
+++ objectModel/Python/setup.py
@@ -77,7 +77,7 @@ class CopyResourcesCommand(distutils.cmd.Command):
 
 setuptools.setup(
     name='commondatamodel-objectmodel',
-    version='1.2.1-a',
+    version='1.2.1-b.3',
     author='Microsoft',
     description='Common Data Model Object Model library for Python',
     url='https://github.com/microsoft/CDM/tree/master/objectModel/Python',
